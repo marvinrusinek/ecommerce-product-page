df8e9598fa1cfa1f3c491a0a76b64ecc909caa9d
Marvin Rusinek <marvinrusinek@gmail.com>
2021-10-23 15:46:20 -0400

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './ecommerce-product/containers/header/header.component';
+import { MainComponent } from './ecommerce-product/containers/main/main.component';
+import { FooterComponent } from './ecommerce-product/containers/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
